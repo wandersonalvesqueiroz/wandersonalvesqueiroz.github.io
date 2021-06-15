b9dbab31e5f777c3fb6453d7f4f1368e81ad27b7
Wanderson <wandersonmg18@gmail.com>
2021-06-14 22:45:59 -0300

    Add cv

---
+++ src/data.js
@@ -0,0 +1,205 @@
+module.exports = {
+  icon: [
+    {
+      size: 16,
+      url: 'https://mazipan.github.io/favicon/favicon-16x16.png',
+    }
+  ],
+  pageTitle: 'Wanderson',
+  pageDesc: 'Currículo Wanderson Alves de Queiroz',
+  profile: {
+    name: 'Wanderson Alves',
+    title: 'Analista Desenvolvedor Back/Frontend',
+    photo: './src/img/wanderson.jpg',
+    city: 'Belo Horizonte, Brasil',
+    phone: '+55 (31) 99318-2668',
+    mail: 'wandersonmg18@gmail.com'
+  },
+  social: {
+    facebook: {
+      name: 'wandersonmg18',
+      link: 'https://web.facebook.com/wandersonmg18'
+    },
+    linkedin: {
+      name: 'in/wandersonalvesqueiroz',
+      link: 'https://www.linkedin.com/in/wandersonalvesqueiroz'
+    },
+    github: {
+      name: 'wandersonalvesqueiroz',
+      link: 'https://github.com/wandersonalvesqueiroz'
+    }
+  },
+  summary: `Desejo trabalhar como Desenvolvedor, para aprimorar meus conhecimentos e
+            crescer na área de TI, meu objetivo é conciliar conhecimentos adquiridos com
+            disposição para novos desafios, visando o crescimento empresarial.`,
+  experiences: [
+    {
+      company: 'Reply',
+      title: 'Desenvolvedor PHP Sênior',
+      period: '10/2018 – ATUAL',
+      jobdesc: [
+        'Desenvolver e corrigir sistema baseado no framework Yii com '+
+        'Bootstrap, DHTMLX e ferramenta Javascript própria adicionando '+
+        'implementando códigos e funcionalidades com auxílio de jQuery e Javascript.',
+        'Dar suporte ao cliente. Utilização de Git para versionamentos e Banco de dados '+
+        'Oracle.'
+      ],
+    },
+    {
+      company: 'SDrummond Soluções em Tecnologia Eirelli',
+      title: 'Programador PHP',
+      period: '(09/2013 – 10/2018)',
+      jobdesc: [
+        'Desenvolver sites em CMS Joomla adicionando implementando'+
+        'códigos e personalizando templates e funcionalidades com auxílio de jQuery e'+
+        'Javascript, além de desenvolver layouts com Photoshop e Illustrator.',
+        'Customizar e otimizar sites PHP, HTML5 e CSS3. Utilização de SVN Tortoise'+
+        'para versionamentos. Configuração de Servidores de hospedagem para'+
+        'websites e e-mails.'
+      ],
+    },
+    {
+      company: 'Bijouterias Fio de Ouro',
+      title: 'Técnico em Informática',
+      period: '(06/2012 - 06/2013)',
+      jobdesc: [
+        'Administrar redes, dar suporte a computadores, impressoras e outros'+
+        'equipamentos, treinar usuários alterar e corrigir aplicações.'
+      ],
+    },
+    {
+      company: 'Attps Informática',
+      title: 'Estagiário de Implantação',
+      period: '(01/2012 - 06/2012)',
+      jobdesc: [
+        'Corrigir e alterar sistema interligado aos bancos de dados Oracle e'+
+        'SQL Server, realizar testes e homologação.'
+      ],
+    },
+    {
+      company: 'Companhia do Strass',
+      title: 'Gerente',
+      period: '(12/2006 - 07/2010)',
+      jobdesc: [
+        'Planejar, organizar e supervisionar todas as atividades comerciais da'+
+        'empresa, visando a atingir os objetivos estabelecidos de volume de vendas,'+
+        'participação de mercado e qualidade do atendimento.'
+      ],
+    }
+  ],
+  educations: [
+    {
+      name: 'FAMINAS - BH',
+      degree: 'Bacharelado',
+      period: '2010 - 2014',
+      faculty: 'Sistemas de Informação'
+    }
+  ],
+  projects: [
+    {
+      title: 'Formação Java',
+      company: 'Treinar Minas',
+      period: '(01/04/2016 – 05/05/2016)'
+    },
+    {
+      title: 'Angular JS',
+      company: 'Framework',
+      period: '(01/06/2016 – 20/06/2016)'
+    }
+  ],
+  dbs: [
+    {
+      name: 'Oracle',
+      progress: 80
+    },
+    {
+      name: 'MySQL',
+      progress: 70
+    },
+    {
+      name: 'SQL Server',
+      progress: 50
+    },
+    {
+      name: 'Postgres',
+      progress: 40
+    }
+  ],
+  skills: [
+    {
+      name: 'PHP',
+      progress: 100
+    },
+    {
+      name: 'Javascript | jQuery | Ajax',
+      progress: 80
+    },
+    {
+      name: 'CSS3',
+      progress: 90
+    },
+    {
+      name: 'HTML5',
+      progress: 100
+    },
+    {
+      name: 'Java',
+      progress: 60
+    }
+  ],
+  frameworks: [
+    {
+      name: 'Yii',
+      progress: 70
+    },
+    {
+      name: 'Laravel',
+      progress: 50
+    },
+    {
+      name: 'Codeigniter',
+      progress: 60
+    },
+    {
+      name: 'Joomla',
+      progress: 90
+    },
+    {
+      name: 'Wordpress',
+      progress: 40
+    },
+    {
+      name: 'Node JS',
+      progress: 50
+    },
+    {
+      name: 'React',
+      progress: 50
+    },
+    {
+      name: 'React Native',
+      progress: 20
+    },
+    {
+      name: 'Angular JS',
+      progress: 20
+    },
+    {
+      name: 'Primeface',
+      progress: 30
+    }
+  ],
+  others: [
+    {
+      name: 'Photoshop | Illustrator | InDesign',
+      progress: 70
+    },
+    {
+      name: 'Projetos e Instalações de Redes | Intranet',
+      progress: 60
+    }
+  ],
+  languages: [
+    'Inglês (Técnico)'
+  ]
+}
